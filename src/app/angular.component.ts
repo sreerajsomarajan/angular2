b7875da06a5a622a7dea1f4f0b2fd91b4a18bd61
Sreeraj S <sreeraj@mobomo.com>
2016-11-24 17:59:37 +0530

    Angular 2 base code
    Signed-off-by: Sreeraj S <developer.sreerajsomarajan@gmail.com>

--- src/app/angular.component.ts
+++ src/app/angular.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CoursesComponent } from './courses.component';
 
 @Component({
   moduleId: module.id,
   selector: 'angular-app',
   templateUrl: 'angular.component.html',
-  styleUrls: ['angular.component.css']
+  styleUrls: ['angular.component.css'],
+  directives: [CoursesComponent]
 })
 export class AngularAppComponent {
-  title = 'angular works!';
+  title = 'angular works well in the browser!';
 }
---
+++ src/app/courses.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core'
+import {CourseService} from './course.service'
+
+@Component({
+  selector: 'courses',
+  template: `<h2>Courses</h2>
+             {{ title }}
+             <ul>
+               <li *ngFor="let course of courses">{{ course }}</li>
+             </ul>`,
+  providers: [CourseService]
+})
+export class CoursesComponent {
+  title = 'This is the page title'
+  courses;
+
+  constructor(courseService: CourseService) {
+    this.courses = courseService.getCourses();
+  }
+}
